295544c8a8b4dab011fd0b262ae7f696f62363ef
skwd1013 <43905686+skwd1013@users.noreply.github.com>
2023-05-26 19:51:50 +0900

    Create var003.java

---
+++ work/var003.java
@@ -0,0 +1,18 @@
+
+public class VarEx4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'A';
+		System.out.println(ch);
+		int i ='A';
+		System.out.println(i);
+		String str ="";
+		String str2 ="AB";
+		String str3 ="123";
+		String str4 =str2+str3;
+		System.out.println(str4);
+		
+	}
+
+}
