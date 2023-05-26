e190deaa8f2f5b6c2dbd704cd4608bf6d748738f
skwd1013 <43905686+skwd1013@users.noreply.github.com>
2023-05-26 17:01:21 +0900

    Create var_002.java

---
+++ work/var_002.java
@@ -0,0 +1,31 @@
+
+public class VarEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean power = true;
+
+		byte b =127; //-128 ~ 127
+		System.out.println(power);
+		System.out.println(b);
+		int oct =010; //8진수 8
+		int hex = 0x10; // 16진수16
+		System.out.println(oct);
+		System.out.println(hex);
+
+		long l =10000000000L;
+		System.out.println(l);
+		float f = 3.14f;
+		double d = 3.14f;
+
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(10.0);
+		System.out.println(0.10);
+		System.out.println(10f);
+		System.out.println(1e3);
+
+
+	}
+
+}
