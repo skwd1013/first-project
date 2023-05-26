39ce3556ef7b25a4d63b8a5fefddb543c92f413d
skwd1013 <43905686+skwd1013@users.noreply.github.com>
2023-05-26 19:58:46 +0900

    Create var004.java

---
+++ work/var004.java
@@ -0,0 +1,20 @@
+
+public class VarEx5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=20;
+		int temp;
+		
+		temp=x;
+		x=y;
+		y=temp;
+		
+		System.out.println(x);
+		System.out.println(y);
+			
+		
+	}
+
+}
